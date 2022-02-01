296c393fc84f9f98917d7e2a6c105f8013e2b817
nacho <nachoherrero93@hotmail.com>
2022-02-01 16:00:09 -0300

    agrego google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,32 @@ import {
 } from "@chakra-ui/react";
 import "../normalize.css";
 
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   );
+// }
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
