8c7b4dd373455cdd7b78e599e40d1e1faf9f1171
nacho <nachoherrero93@hotmail.com>
2022-02-01 16:47:21 -0300

    update documents

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
         <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-PDG954ZBWB`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-MGM6C356VF`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-PDG954ZBWB', {
+            gtag('config', 'G-MGM6C356VF', {
               page_path: window.location.pathname,
             });
           `,
